652eaf939064c7b5f0bd98a55a05bc933d305dca
Giuseppeworkjujujj <you@example.com>
2023-02-03 22:00:28 +0100

    start

--- src/components/TDue.vue
+++ src/components/TDue.vue
@@ -1,95 +1,3 @@
-<!-- <template>
-
-  <div class="row">
-      <div class="col-12 d-flex justify-content-center">
-        <div class="row-2 d-flex justify-content-center">
-          <h1 class="">Il tuo posto per parlare</h1>
-        </div>
-        <div class="row-3 d-flex justify-content-center">
-          <p>Che tu faccia parte di un club scolastico, gruppo gamer, comunità artistica mondiale o semplicemente di un piccolo gruppo di amici che vuole passare il tempo assieme, Discord rende più facile chiaccherare ogni giorno e ritrovarsi più spesso.</p>
-        </div>
-        <div class="row-4 d-flex justify-content-center">
-          <button class="btn btn-light mx-2 ">Scarica per Mac</button>
-          <button class="btn btn-dark mx-2">Apri discord nel tuo browser</button>
-        </div>
-      </div>
- </div>
-</template>
-
-<script>
-export default {
-  name: 'TDueSito',
-}
-</script>
-
-Add "scoped" attribute to limit CSS to this component only
-<style lang='scss' scoped>
- .row{
-    height: 510px;
-    width: 100%;
-    
-    margin-left: 0px;
-    background-image: url("../assets/jumbo.png");
-    background-size: cover;
-    background-repeat:no-repeat;
-    background-position: left  top  ;
-    background-color: #265ee9;
-       .row-2{
-        
-        position: absolute;
-        width: 50%;
-        top: 205px;
-        h1{
-          font-size: 60px;
-          color: white;
-        }
-       } 
-       .row-3{
-        position: absolute;
-        width: 50%;
-        top: 290px;
-        text-align: center;
-        p{
-          font-size: medium;
-          color: white;
-        }
-       }
-       .row-4{
-        height: 45px;
-        position: absolute;
-        width: 80%;
-        top: 390px;
-       }
-       @media screen and (max-width:1220px){
-        h1{
-        position: absolute;
-        top:-50px;
-        text-align: center;
-       }
-       p{
-        position: absolute;
-        top: 30px;
-        width: 80%;
-       }
-       button{
-        position: absolute;
-        top:80px;
-       }
-      }
-      @media screen and (max-width:815px){
-        .row{
-        background-size: 1500px 100%;
-       }
-        img{
-         width: 80%;
-          height: 450px;
-        }
-      }
-      }
-   
-   
-</style> -->
-
 <template>
 
   <div class="row">
@@ -105,8 +13,7 @@ Add "scoped" attribute to limit CSS to this component only
           <button class="btn btn-dark mx-1 d-flex align-items-center">Apri discord nel tuo browser</button>
         </div>
       </div>
-    <!-- </div> -->
- </div>
+  </div>
 </template>
 
 <script>
@@ -156,7 +63,7 @@ export default {
        @media screen and (max-width:1220px){
         .col-12-3{
         position: absolute;
-        width: 50%;
+        width: 60%;
         top: 290px;
         text-align: center;
         p{
@@ -166,7 +73,6 @@ export default {
        }
        .col-12-4{
         position: absolute;
-        // width: 100%;
         top: 450px;
        }
         h1{
@@ -181,7 +87,7 @@ export default {
        }
 
       }
-      @media screen and (max-width:815px){
+      @media screen and (max-width:800px){
         .row{
         background-size: 1500px 100%;
        }
@@ -207,15 +113,9 @@ export default {
        }
        .col-12-4{
         visibility: hidden;
-       }
-        
-       
-        img{
-         width: 80%;
-          height: 450px;
-        }
       }
-      }
+  }
+}
    
    
 </style>
